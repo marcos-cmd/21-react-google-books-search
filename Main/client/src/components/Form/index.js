61941a10fc66ebfb613ec016c12c04ae32dcdf80
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-12-18 18:55:21 -0800

    styling change

--- Main/client/src/components/Form/index.js
+++ Main/client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="Search for a book"
           name="q"
           onChange={handleInputChange}
           required
